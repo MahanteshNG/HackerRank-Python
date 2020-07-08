06ad94dd2c04dc68600cc844b0ab69dd09603c6c
MAHANTESH GHULAPPANAVAR <mahantng23@gmail.com>
2020-07-08 21:40:40 +0530

    Create ArithmeticOperators.py

---
+++ Introduction/ArithmeticOperators.py
@@ -0,0 +1,8 @@
+import math
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    if 1<=a and b<=(math.pow(10,10)):
+        print(a+b)
+        print(a-b)
+        print(a*b)
