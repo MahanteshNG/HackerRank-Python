abc716784c050580e107c8d2ecabac609d1fb32e
MAHANTESH GHULAPPANAVAR <mahantng23@gmail.com>
2020-07-08 21:51:51 +0530

    Create NestedLists.py

---
+++ BasicDataTypes/NestedLists.py
@@ -0,0 +1,24 @@
+s=[]
+for studentsnumber in range(int(input())):
+    s.append([input(),float(input())])
+minimum=s[0][1]
+for each in s:
+    if minimum>=each[1]:
+        minimum=each[1]
+for each in s:
+    if each[1]==minimum:
+        s.remove(each)
+sl=[]
+minimum=s[0][1]
+for each in s:
+    if minimum>each[1]:
+        minimum=each[1]
+for each in s:
+    if each[1]==minimum:
+        sl.append(each[0])
+sl.sort()
+for i in range(len(sl)):
+    if i+1==len(sl):
+        print(sl[i])
+    else:
+        print(sl[i],end='\n')
